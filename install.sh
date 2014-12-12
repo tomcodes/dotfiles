0cfdbfebd8de5b615e78200d2c2423035734e9ad
tomcodes <tomcodes@users.noreply.github.com>
2014-12-12 14:06:01 +0100

    [Fix] - Fix Prezto post install

--- install.sh
+++ install.sh
@@ -40,8 +40,8 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
         echo "====================================="
         echo "Be sure to run the following command:"
         echo "setopt EXTENDED_GLOB
-              for rcfile in \"${ZDOTDIR:-$HOME}\"/.zprezto/runcoms/^README.md(.N); do
-                  ln -s \"$rcfile\" \"${ZDOTDIR:-$HOME}/.${rcfile:t}\"
+              for rcfile in \"\$\{ZDOTDIR:-$HOME\}\"/.zprezto/runcoms/^README.md(.N); do
+                  ln -s \"$rcfile\" \"$\{ZDOTDIR:-\$HOME\}/.\$\{rcfile:t\}\"
               done"
         echo "====================================="
 
